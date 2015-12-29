76c08d5b85ff6d65f1cb35a3bde5f4196efc2d6d
Denis O <denis@universal-development.com>
2015-12-29 15:24:56 +0200

    Automatically generated controller paths pointed to /admin/*

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -1,7 +1,7 @@
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+        "/admin/$controller/$action?/$id?(.$format)?"{
             constraints {
                 // apply constraints here
             }
