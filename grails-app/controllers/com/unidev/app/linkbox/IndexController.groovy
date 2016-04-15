20aba35d18b528a7c89c462287991b01b70b89d6
Denis <denis@universal-development.com>
2016-04-15 12:15:47 +0300

    Link group sorting by weight

--- grails-app/controllers/com/unidev/app/linkbox/IndexController.groovy
+++ grails-app/controllers/com/unidev/app/linkbox/IndexController.groovy
@@ -4,8 +4,7 @@ package com.unidev.app.linkbox
 class IndexController {
 
     def index() {
-        def linkGroupList = LinkGroup.findAll()
-
+        def linkGroupList = LinkGroup.findAll(sort: 'weight', order: 'desc')
         [linkGroupList: linkGroupList]
     }
 }
